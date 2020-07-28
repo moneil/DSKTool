dc349db9aa215df1f5b5ee6fe2108be89de99fb5
Mark O'Neil <zeretic@gmail.com>
2020-07-27 21:14:33 -0400

    Fix config issue

--- config_template.py
+++ config_template.py
@@ -6,9 +6,13 @@
 # KEY = adict['learn_rest_key']
 # SECRET = adict['learn_rest_key']
 # LEARNFQDN = adict['learn_rest_fqdn']
+# django_secret_key comes from here: https://djskgen.herokuapp.com
+
 adict = {
     "learn_rest_fqdn" : "your.learnserver.net",
     "learn_rest_key" : "Your REST API KEY GOES HERE NOT Mine",
     "learn_rest_secret" : "Your REST API SECRET GOES HEREer",
+    "django_secret_key" : 'a secret key from the above URL',
+    "django_allowed_hosts" : "127.0.0.1 localhost .ngrok.io .herokuapp.com [::1]"
 }
 
